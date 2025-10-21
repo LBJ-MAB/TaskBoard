faa5e7808e6f1f3d91c0cf9538f5e2b33b638759
KEYFSLTD\Laurie.Bassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-10-21 13:06:35 +0100

    wrote one unit test to check implementation of IResult

--- Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45f87b0dc5a5b8a467d6ab9419f48649af535432")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f241c1a663558b06044ce154b70db289c6e241")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
