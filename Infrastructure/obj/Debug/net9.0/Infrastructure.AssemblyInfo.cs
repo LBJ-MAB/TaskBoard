8e97aee02b17001e944a5e3660b8955f56f3d294
LaurieBassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-10-23 08:31:26 +0100

    adding a class for logging correlation Id middleware

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78279f99d72ff501d824dd8adcc27eaf1b4b8aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+779c1089b0fea6707f2aa182830eca4cae66b67a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
